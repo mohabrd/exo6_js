15b4e7c20c0223b88414fe62cd55f07bb628f522
mohabrd <bezrhoudimohamed@gmail.com>
2022-12-21 13:04:17 +0100

    boucle do while 2

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,19 @@
 // BOUCLE #1.2
 
-for (let index = 1; index <= 20; index++) {
-    modulo = index %3;
-    (modulo==0) && console.log(index);
-}
+// for (let index = 1; index <= 20; index++) {
+//     modulo = index %3;
+//     (modulo==0) && console.log(index);
+// }
+
+// BOUCLE #2.2
+
+let nbr;
+do {
+    nbr= +prompt("Saisie un nombre pair!");
+
+    if (nbr % 2 != 0) {
+        alert("Ce nombre n'est pas pair. REESSAYE");
+      }
+
+} while (nbr % 2);
+nbr= alert("BIEN JOUER")
